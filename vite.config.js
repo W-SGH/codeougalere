8dd8e6b497d728dc7ab8d256285202609b3373b8
Walid Seghouane <waldo@Mac.lan>
2026-03-25 22:25:04 +0100

    Remove vite-plugin-pwa (vite@8 incompatible), use native manifest.json

--- vite.config.js
+++ vite.config.js
@@ -1,57 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'icons.svg'],
-      manifest: {
-        name: 'Permis ou Galère',
-        short_name: 'PermisGalère',
-        description: 'Réussissez votre Code de la Route du premier coup',
-        theme_color: '#f5c400',
-        background_color: '#ffffff',
-        display: 'standalone',
-        start_url: '/',
-        lang: 'fr',
-        icons: [
-          {
-            src: '/pwa-192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/pwa-512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: '/pwa-512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
-        ],
-      },
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,svg,png,jpg,jpeg,woff2}'],
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'google-fonts',
-              expiration: { maxEntries: 10, maxAgeSeconds: 60 * 60 * 24 * 365 },
-            },
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [react()],
   server: {
     host: true,
   },
